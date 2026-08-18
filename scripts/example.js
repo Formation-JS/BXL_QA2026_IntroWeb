31e70890622cf65082d6d98594b88fa65621eddb
Aurélien <aurelien.strimelle@bstorm.be>
2026-08-18 13:50:41 +0200

    Game form 🐍

--- scripts/example.js
+++ scripts/example.js
@@ -1,1 +1,38 @@
-console.log("Hello World !");
+console.log("Jeu du pendu !");
+
+//! Récuperation du formulaire dans le DOM de la page
+const gameForm = document.getElementById('game-form');
+const msgGameForm = document.getElementById('message-game-form');
+
+//! Variable de stockage
+const letterAlreadySubmit = [];
+
+//! Réaction à la validation du formulaire
+gameForm.addEventListener('submit', function (event) {
+    // Annulation du comportement par defaut => Refresh
+    event.preventDefault();
+
+    // Récuperer la valeur (depuis le form)
+    // - La balise "input" via son "name"
+    const userInput = gameForm['user-input'];
+    // - On lit la valeur contenu
+    const letter = userInput.value.toUpperCase();
+    console.log(letter);
+
+    // Traitement de la lettre
+    if(letter.length !== 1) {
+        msgGameForm.textContent = 'La lettre invalide';
+    }
+    else if(letterAlreadySubmit.includes(letter)) {
+        msgGameForm.textContent = `La lettre ${letter} a déjà été proposé ! Boulet ♥`;
+    }
+    else {
+        letterAlreadySubmit.push(letter);
+        msgGameForm.textContent = `La lettre selectionnée est ${letter}`;
+
+        // TODO...
+    }
+
+    // Efface la valeur de l'input
+    userInput.value = '';
+});
